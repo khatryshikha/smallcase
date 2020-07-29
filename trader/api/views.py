149e62e853caddbc5094a013062d421c22bb47d8
khatryshikha <shikhakhatry2612@gmail.com>
2020-07-30 01:20:18 +0530

    feat(api): Mongo ORM, URL Routes

--- trader/api/views.py
+++ trader/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.views.generic import View
+from api.models import Testing
 
-# Create your views here.
+class TradeView(View):
+    def get(self, request):
+        testing_docs = Testing.objects()
+        print(testing_docs)
+        print("inside get")
+        return HttpResponse("hello get")
+
+    def post(self, request):
+        try:
+            page = Testing(title='Using MongoEngine')
+            page.save()
+            return HttpResponse("hello post")
+        except Exception as exc:
+            print(str(exc))
+            return "uh oh"
