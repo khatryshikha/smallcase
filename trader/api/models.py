f36a3c5ad692f354dc78008df6793ec65b536991
khatryshikha <shikhakhatry2612@gmail.com>
2020-07-31 14:22:15 +0530

    feat(models.py) : Portfolio schema changed

--- trader/api/models.py
+++ trader/api/models.py
@@ -3,17 +3,26 @@ from mongoengine import *
 import datetime
 
 
+class Trades(EmbeddedDocument):
+    operations = StringField(max_length=200, required=True)
+    shares = IntField(min_value=0)
+    buy_price = DecimalField(default=0)
+    timestamp = DateTimeField(default=datetime.datetime.now)
+
+
 class Portfolio(Document):
     ticker_symbol = StringField(max_length=200, required=True)
     avg_buy_price = DecimalField()
-    shares = IntField(min_value=0)
-    trades = ListField(StringField(), default=list)
-    timestamp = DateTimeField(default=datetime.datetime.now)
+    total_shares = IntField(min_value=0)
+    trades = EmbeddedDocumentListField(Trades)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
+
     meta = {"indexes": ["ticker_symbol"]}
 
     def is_valid(self):
         return (
             self.ticker_symbol != ""
             and self.avg_buy_price != ""
-            and (self.shares != "" and self.shares > 0)
+            and (self.total_shares != "" and self.total_shares > 0)
         )
