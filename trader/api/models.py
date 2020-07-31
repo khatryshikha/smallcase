e1a2ccd224436f447673a6b3277e68f24b121678
khatryshikha <shikhakhatry2612@gmail.com>
2020-07-31 16:28:35 +0530

    feat(view.py) : API changes according to schema

--- trader/api/models.py
+++ trader/api/models.py
@@ -1,28 +1,31 @@
 # Create your models here.
-from mongoengine import *
 import datetime
+import uuid
+
+from mongoengine import *
 
 
 class Trades(EmbeddedDocument):
-    operations = StringField(max_length=200, required=True)
-    shares = IntField(min_value=0)
+    operation = StringField(max_length=200, required=True)
+    ticker_symbol = StringField(max_length=200, required=True)
+    shares_count = IntField(min_value=0)
     buy_price = DecimalField(default=0)
     timestamp = DateTimeField(default=datetime.datetime.now)
-
-
-class Portfolio(Document):
-    ticker_symbol = StringField(max_length=200, required=True)
-    avg_buy_price = DecimalField()
-    total_shares = IntField(min_value=0)
-    trades = EmbeddedDocumentListField(Trades)
-    created_at = DateTimeField(default=datetime.datetime.now)
-    updated_at = DateTimeField(default=datetime.datetime.now)
 
     meta = {"indexes": ["ticker_symbol"]}
 
     def is_valid(self):
         return (
             self.ticker_symbol != ""
-            and self.avg_buy_price != ""
-            and (self.total_shares != "" and self.total_shares > 0)
+            and self.buy_price != ""
+            and (self.shares_count != "" and self.shares_count > 0)
         )
+
+
+class Portfolio(Document):
+    uid = UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    average_buy_price = DecimalField()
+    total_shares_count = IntField(min_value=0)
+    trades = EmbeddedDocumentListField(Trades)
+    created_at = DateTimeField(default=datetime.datetime.now)
+    updated_at = DateTimeField(default=datetime.datetime.now)
--- trader/api/views.py
+++ trader/api/views.py
@@ -1,171 +1,230 @@
 import datetime
 
-from django.http import Http404, HttpResponse, JsonResponse, QueryDict
+from django.http import JsonResponse, QueryDict
 from django.shortcuts import redirect, render
 from django.views.generic import View
 
-from api.models import Portfolio
+from api.models import Portfolio, Trades
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title="Portfolio Tracker APIs")
 
 
 class TradeView(View):
-
     # Adding the trade details to DB
     def post(self, request):
         try:
-            # Data of the trade
-            ticker = str(request.POST.get("ticker_symbol", ""))
-            buy_price = float(request.POST.get("avg_buy_price", ""))
-            no_shares = int(request.POST.get("shares", ""))
-            trade = f"Bought {no_shares} shares of {ticker} at {buy_price}"
+            # Input data of the trade
+            data_ticker = (str(request.POST.get("ticker_symbol", ""))).upper()
+            data_buy_price = float(request.POST.get("buy_price", ""))
+            data_shares_count = int(request.POST.get("shares_count", ""))
+            data_trade_operation = (
+                str(request.POST.get("trade_operation", ""))
+            ).capitalize()
 
-            # Checking protofolio if the entry for same security exist.
-            is_Portfolio_exist = Portfolio.objects(ticker_symbol=ticker)
+            # Creating a embedded document object for Portfolio object
+            trade_obj = Trades(
+                operation=data_trade_operation,
+                ticker_symbol=data_ticker,
+                shares_count=data_shares_count,
+                buy_price=data_buy_price,
+            )
 
-            if not len(is_Portfolio_exist):
-                # Adding the trade into the DB
-                portfolio = Portfolio(
-                    ticker_symbol=ticker,
-                    avg_buy_price=buy_price,
-                    shares=no_shares,
-                    trades=[trade],
-                )
-
-                # Validation for NULL value or shares count less than 0
-                if portfolio.is_valid():
-                    portfolio.save()
-
+            # Validation check
+            if not trade_obj.is_valid():
                 return JsonResponse(
                     {
-                        "code": "0",
-                        "status": "success",
-                        "Message": "Successfully added the shares",
-                    }
+                        "status": "fail",
+                        "message": "Invalid input! Black fields and negative shares values are not allowed",
+                    },
+                    status=400,
                 )
+
+            # Checking the existance of the security for the given ticker symbol
+            portfolio_obj_exist = Portfolio.objects(
+                trades__match={"ticker_symbol": data_ticker}
+            )
+
+            if not len(portfolio_obj_exist):
+                # If till yet no trade for given security, add the trade and updating portfolio
+                portfolio_obj = Portfolio(
+                    average_buy_price=data_buy_price,
+                    total_shares_count=data_shares_count,
+                    trades=[trade_obj],
+                )
+                portfolio_obj.save()
+
             else:
-                self.update(
-                    ticker_symbol=ticker,
-                    avg_buy_price=buy_price,
-                    shares=no_shares,
-                    is_add=True,
+                # else update the existing security and add new trade to it
+                portfolio_shares_count = int(
+                    portfolio_obj_exist[0]["total_shares_count"]
                 )
-                return JsonResponse(
-                    {
-                        "code": "0",
-                        "status": "success",
-                        "Message": "Security already exist, updated successfully!",
-                    }
+                portfolio_average_buy_price = float(
+                    portfolio_obj_exist[0]["average_buy_price"]
                 )
+                portfolio_trades_list = portfolio_obj_exist[0]["trades"]
 
-        except Exception as exc:
-            print(str(exc))
-            return JsonResponse(
-                {"code": "1", "status": "failed", "Message": "Failed to add the shares"}
-            )
+                # Check for trade operation
+                if data_trade_operation == "Buy":
+                    cumulative_shares = portfolio_shares_count + data_shares_count
+                    cumulative_buy_price = (
+                        portfolio_average_buy_price * portfolio_shares_count
+                    ) + (data_buy_price * data_shares_count)
+                    average_buy_price = cumulative_buy_price / cumulative_shares
+                elif data_trade_operation == "Sell":
+                    cumulative_shares = portfolio_shares_count - data_shares_count
+                    average_buy_price = portfolio_average_buy_price
 
-    def put(self, request):
-        try:
-            # Updated form details
-            data = QueryDict(request.body)
+                portfolio_trades_list.append(trade_obj)
 
-            self.update(
-                ticker_symbol=str(data.get("ticker_symbol")),
-                avg_buy_price=float(data.get("avg_buy_price")),
-                shares=int(data.get("shares")),
-                is_add=False,
-            )
+                # Updating the trade in DB
+                portfolio_obj_exist.update(
+                    set__total_shares_count=cumulative_shares,
+                    set__average_buy_price=average_buy_price,
+                    set__trades=portfolio_trades_list,
+                )
 
             return JsonResponse(
-                {
-                    "code": "0",
-                    "status": "success",
-                    "Message": "Successfully updated the share",
-                }
+                {"status": "success", "message": "Trade successfully done"}, status=200
             )
+
         except Exception as e:
             print(str(e))
             return JsonResponse(
-                {
-                    "code": "1",
-                    "status": "failed",
-                    "Message": "Failed to update the share",
+                {"status": "fail", "message": "Trade failed due to server error"},
+                status=500,
+            )
+
+    # Deleting the trade
+    def delete(self, request):
+        try:
+            # Input data of the trade
+            data = QueryDict(request.body)
+            data_ticker = (str(data.get("ticker_symbol"))).upper()
+            data_shares_count = int(data.get("shares_count"))
+            data_buy_price = float(data.get("buy_price"))
+            data_trade_operation = (str(data.get("trade_operation"))).capitalize()
+
+            # Querying the document security for given trade ticker
+            trade_obj = Portfolio.objects(
+                trades__match={
+                    "ticker_symbol": data_ticker,
+                    "shares_count": data_shares_count ,
+                    "buy_price": data_buy_price,
                 }
             )
 
-    # Delete the document for given ticker symbol
-    def delete(slef, request):
-        try:
-            data = QueryDict(request.body)
-            Portfolio.objects(ticker_symbol=str(data.get("ticker_symbol"))).delete()
+            if not len(trade_obj):
+                return JsonResponse(
+                    {"status": "fail", "message": "No such trade exist"}, status=404,
+                )
+
+            security_dict = trade_obj[0]["uid"]
+
+            # deleting the trade from that security
+            trade_obj.update_one(
+                pull__trades__operation=data_trade_operation
+            )
+
+            portfolio_obj = Portfolio.objects(uid=security_dict)
+
+            if not len(trade_obj):
+                portfolio_obj.delete()
+            else:
+                changed_total_shares = int(
+                    portfolio_obj[0]["total_shares_count"]
+                ) - data_shares_count
+                changed_average_buy_price = float(
+                    portfolio_obj[0]["average_buy_price"]
+                ) - float(data_buy_price * data_shares_count)
+
+                portfolio_obj.update(
+                    set__average_buy_price=changed_average_buy_price,
+                    set__total_shares_count=changed_total_shares,
+                )
 
             return JsonResponse(
-                {
-                    "code": "0",
-                    "status": "success",
-                    "Message": "Successfully deleted the share",
-                }
+                {"status": "success", "message": "Trade deletion Successfully"},
+                status=200,
             )
+
         except Exception as e:
             return JsonResponse(
                 {
-                    "code": "1",
-                    "status": "failed",
-                    "Message": "Failed to delete the share",
+                    "status": "fail",
+                    "message": "Trade deletion failed due to server error",
+                },
+                status=500,
+            )
+
+    # Updating the existing trade with only changes in shares and buy_price
+    def put(self, request):
+        try:
+            # Input data contains both old details and to update details
+            data = QueryDict(request.body)
+            data_ticker = (str(data.get("ticker_symbol"))).upper()
+            data_shares_count = int(data.get("shares_count"))
+            data_buy_price = float(data.get("buy_price"))
+            data_trade_operation = (str(data.get("trade_operation"))).capitalize()
+
+            # Querying the trade for given details
+            trade_obj = Portfolio.objects(
+                trades__match={
+                    "ticker_symbol": data_ticker,
+                    "shares_count": data_shares_count,
+                    "buy_price": data_buy_price,
+                    "operation": data_trade_operation,
                 }
             )
 
-    def update(self, **kwargs):
+            # Update only when trade exists
+            if len(trade_obj):
 
-        # Input data
-        data_shares = int(kwargs["shares"])
-        data_avg_buy_price = float(kwargs["avg_buy_price"])
-        data_ticker = str(kwargs["ticker_symbol"])
-        data_is_add = kwargs["is_add"]
+                security_dict = trade_obj[0]["uid"]
 
-        # Retrieving existing data from DB
-        Portfolio = Portfolio.objects.get(ticker_symbol=data_ticker)
-
-        Portfolio_shares = int(Portfolio["shares"])
-        Portfolio_avg_buy_price = float(Portfolio["avg_buy_price"])
-        Portfolio_trade = Portfolio["trades"]
-
-        # if update is via add functionality
-        if data_is_add:
-            shares = Portfolio_shares + data_shares
-            buy_price = (Portfolio_avg_buy_price * Portfolio_shares) + (
-                data_avg_buy_price * data_shares
-            )
-            avg_buy_price = buy_price / shares
-            trade = (
-                f"Bought {data_shares} shares of {data_ticker} at {data_avg_buy_price}"
-            )
-        else:
-            # else update to the existing values
-            shares = data_shares
-
-            # if shares are sold
-            if Portfolio_shares > data_shares:
-                trade = f"Sold {Portfolio_shares - data_shares} shares of {data_ticker} at {data_avg_buy_price}"
-                avg_buy_price = Portfolio_avg_buy_price
-            else:
-                # else bought the new shares
-                buy_price = (Portfolio_avg_buy_price * Portfolio_shares) + (
-                    data_avg_buy_price * (data_shares - Portfolio_shares)
+                update_data_share_count = data.get(
+                    "updated_shares_count", data_shares_count
                 )
-                avg_buy_price = buy_price / shares
-                trade = f"Bought {data_shares - Portfolio_shares} shares of {data_ticker} at {data_avg_buy_price}"
+                update_buy_price = data.get(
+                    "updated_buy_price", data_buy_price
+                )
 
-        Portfolio_trade.append(trade)
+                trade_obj.update_one(
+                    set__trades__S=Trades(
+                        operation=data_trade_operation,
+                        ticker_symbol=data_ticker,
+                        shares_count=update_data_share_count,
+                        buy_price=update_buy_price,
+                        timestamp=datetime.datetime.now(),
+                    )
+                )
+                portfolio_obj = Portfolio.objects(uid=security_dict)
+                changed_total_shares = int(
+                    portfolio_obj[0]["total_shares_count"]
+                ) - data_shares_count + int(update_data_share_count)
 
-        # Updating the document in DB
-        Portfolio.update(
-            set__shares=data_shares,
-            set__avg_buy_price=avg_buy_price,
-            set__trades=Portfolio_trade,
-            set__timestamp=datetime.datetime.now(),
-        )
+                portfolio_obj.update(
+                    set__total_shares_count=update_data_share_count,
+                )
+                return JsonResponse(
+                    {"status": "success", "message": "Trade updated Successfully"},
+                    status=200,
+                )
+            else:
+                return JsonResponse(
+                    {
+                        "status": "fail",
+                        "message": f'No such trade exist for {data_ticker}',
+                    },
+                    status=404,
+                )
 
-        # Validating the entries
-        if Portfolio.is_valid():
-            return
-        else:
-            raise Exception("Invalid Input")
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "status": "fail",
+                    "message": "Trade update failed due to server error",
+                },
+                status=500,
+            )
