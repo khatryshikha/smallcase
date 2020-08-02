5a7a54762b647439e007d95391b2c8efee341ef2
khatryshikha <shikhakhatry2612@gmail.com>
2020-08-02 23:20:56 +0530

    feat(settings.py) : removed secret key

--- trader/trader/settings.py
+++ trader/trader/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "v4-e$8d2u+7&=a3gg#+2%oom-u!_2%q@b688d=amdfv-i7x116"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
